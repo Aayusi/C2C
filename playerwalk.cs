4cb6678a397f799aa2f1b7eaed4cfc56a74d8f40
Vatsal Rathod <35826109+vatsal-rathod@users.noreply.github.com>
2019-03-24 09:37:37 +0530

    Add files via upload

---
+++ playerwalk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerwalk : MonoBehaviour
+{
+    public int playerspeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+            transform.position = transform.position + Camera.main.transform.forward * playerspeed * Time.deltaTime;
+        
+        
+    }
+}
